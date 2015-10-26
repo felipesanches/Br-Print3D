83909ee35b2989e9b8f241a0c5180dfa2466b49f
Lays Rodrigues <laysrodriguessilva@gmail.com>
2015-10-26 12:18:18 -0200

    Initial commit to port functions and actions to ManualControl Class
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- brprint3d.cpp
+++ brprint3d.cpp
@@ -380,32 +380,7 @@ void BrPrint3D::locate_Arduino(bool b)
 
 
 }
-//This action search for Slic3er manually, if the slic3er isnt installed, the user could user the bin
-void BrPrint3D::on_bt_addSlicer_clicked()
-{
-    QString filename=QFileDialog::getOpenFileName(this, "Open File",QDir::homePath(),"slic3r");
-    ui->cb_Slicer->setItemText(0,"Slic3r");
-    settings.setValue("slic3r",filename);
-    settings.sync();
-    ui->bt_addSlicer->setEnabled(false);
-}
-//This action start the job of slicing
-void BrPrint3D::on_bt_startSlicer_clicked()
-{
-    if(ui->cb_Slicer->currentText()=="Slic3r")
-    {
 
-    }
-    if(ui->cb_Slicer->currentText()=="Cura Engine")
-    {
-
-    }
-}
-//This action kill the job of slicing
-void BrPrint3D::on_bt_killSlicer_clicked()
-{
-
-}
 //This action import a GCODE file to print
 void BrPrint3D::on_bt_import_clicked()
 {
--- manualcontrol.cpp
+++ manualcontrol.cpp
@@ -12,3 +12,30 @@ ManualControl::~ManualControl()
 {
     delete ui;
 }
+
+//This action search for Slic3er manually, if the slic3er isnt installed, the user could user the bin
+void ManualControl::on_bt_addSlicer_clicked()
+{
+    QString filename=QFileDialog::getOpenFileName(this, "Open File",QDir::homePath(),"slic3r");
+    ui->cb_Slicer->setItemText(0,"Slic3r");
+    settings.setValue("slic3r",filename);
+    settings.sync();
+    ui->bt_addSlicer->setEnabled(false);
+}
+//This action start the job of slicing
+void ManualControl::on_bt_startSlicer_clicked()
+{
+    if(ui->cb_Slicer->currentText()=="Slic3r")
+    {
+
+    }
+    if(ui->cb_Slicer->currentText()=="Cura Engine")
+    {
+
+    }
+}
+//This action kill the job of slicing
+void BrPrint3D::on_bt_killSlicer_clicked()
+{
+
+}
