df28414a683198a6610fdf8581306a8298eaf694
Lays Rodrigues <laysrodriguessilva@gmail.com>
2015-10-26 10:29:58 -0200

    Create the QSettings variable on PrinterConfigs File
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- printerconfigs.cpp
+++ printerconfigs.cpp
@@ -14,7 +14,7 @@ PrinterConfigs::~PrinterConfigs()
 }
 
 void PrinterConfigs::init(QSettings settings)
-{
+{   this->settings = settings;
     //Load the previous configs if them exists
     QStringList groups;
     settings.beginGroup("Printer_Configs");
@@ -55,4 +55,45 @@ void PrinterConfigs::init(QSettings settings)
         ui->tb_extruder_vol_max->setText(this->extruderMAXVol);
     }
 
+}
+
+void BrPrint3D::on_bt_saveConfig_clicked() //Salva as configurações de impressoras
+{   bool ok;
+    QString name = QInputDialog::getText(this, tr("Insert the name of config: "),tr("Nome:"),QLineEdit::Normal,"ex.: Graber1",&ok);
+    settings.beginGroup("Printer_Configs");
+    if(ok && !name.isEmpty())
+    {
+        settings.beginGroup(name);
+        //Conexao
+        settings.setValue("Firmware",ui->cb_Firmware->currentText());
+        settings.setValue("ResetOnConnect",ui->ck_resetonConnect->checkState());
+        settings.setValue("ResetonEmergency",ui->ck_resetonEmergency->checkState());
+        settings.setValue("CacheSize", ui->cb_Cache_Size->currentText());
+        settings.setValue("TransmissionRate",ui->cb_Transmition_Rate->currentText());
+        settings.setValue("PrinterLog", ui->ck_logImpressao->checkState());
+        //Impressora
+        settings.setValue("RateMoviment",ui->tb_RateMoviment->text());
+        settings.setValue("FeedZ",ui->tb_FeedZ->text());
+        settings.setValue("ExtruderSpeed",ui->tb_ExtruderSpeed_Sec->text());
+        settings.setValue("ExtruderRetraction",ui->tb_SpeedRetraction->text());
+        settings.setValue("BedTemperature",ui->tb_Table_Temperature->text());
+        settings.setValue("ExtruderTemperature",ui->tb_ExtruderTemperature->text());
+        settings.setValue("AreaX",ui->tb_AreaPrint_X->text());
+        settings.setValue("AreaY",ui->tb_AreaPrint_Y->text());
+        settings.setValue("AreaZ",ui->tb_AreaPrint_Z->text());
+        //Extrusor
+        settings.setValue("ExtruderQnt",ui->cb_Extruder_qnt->currentText());
+        settings.setValue("ExtruderMaxTemp",ui->tb_extruderMaxTemp->text());
+        settings.setValue("BedMaxTemp",ui->tb_bedMaxTemp->text());
+        settings.setValue("VolumeMax",ui->tb_extruder_vol_max->text());
+        settings.endGroup();
+
+    }
+    settings.endGroup();
+    QStringList groups;
+    settings.beginGroup("Printer_Configs");
+    groups = settings.childGroups();
+    settings.endGroup();
+    ui->cb_printer->clear();
+    ui->cb_printer->addItems(groups);
 }
--- printerconfigs.h
+++ printerconfigs.h
@@ -19,6 +19,7 @@ public:
 
 private:
     Ui::PrinterConfigs *ui;
+    QSettings settings;
 
 };
 
