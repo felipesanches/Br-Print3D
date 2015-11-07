b8e4f173d668f6e6013ff43bc517a0925c2a2cbf
Lays Rodrigues <laysrodriguessilva@gmail.com>
2015-11-07 00:50:12 -0200

    Port class to use QVTKWidget2
    Initial port to use QVTKWidget2.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- vtkWidget2.cpp
+++ vtkWidget2.cpp
@@ -1,7 +1,32 @@
 #include "vtkWidget2.h"
 
-vtkWidget2::vtkWidget2()
+vtkWidget2::vtkWidget()
 {
 
 }
+void vtkWidget2::renderSTL(QString pathStl)
+{
 
+    vtkSmartPointer<vtkSTLReader> reader = vtkSmartPointer<vtkSTLReader>::New();
+    reader->SetFileName(pathStl.toStdString().c_str());
+    reader->Update();
+
+    vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputConnection(reader->GetOutputPort());
+
+    vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+
+    vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+    vtkRenderWindowInteractor *r = this->GetRenderWindow()->GetInteractor();
+    vtkRenderWindow* renderWindow = vtkRenderWindow::New();
+    r->SetRenderWindow(renderWindow);
+    renderWindow->AddRenderer(renderer);
+
+    renderer->AddActor(actor);
+    renderer->SetBackground(.0, .5, 1); // Background color green
+
+    renderer->ResetCamera();
+    renderWindow->Render();
+
+}
--- vtkWidget2.h
+++ vtkWidget2.h
@@ -1,11 +1,21 @@
 #ifndef VTKWIDGET2_H
 #define VTKWIDGET2_H
-
+#include <QVTKWidget2.h>
+#include <vtkPolyData.h>
+#include <vtkSTLReader.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkGenericOpenGLRenderWindow.h>
 
 class vtkWidget2 : public QVTKWidget2
 {
 public:
     vtkWidget2();
+    void renderSTL(QString path);
 };
 
 #endif // VTKWIDGET2_H
